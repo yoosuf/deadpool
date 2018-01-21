4abfeb74d56a9b3e26fd827cca79817e1dd69988
Yoosuf <mayoosuf@gmail.com>
2018-01-21 13:38:37 +0530

    wip

---
+++ app/Http/Controllers/Api/V1/ProvidersController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+
+class ProvidersController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+
+    public function index()
+    {
+
+    }
+
+
+}
