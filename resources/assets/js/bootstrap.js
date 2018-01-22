ddc2db7403f73b8d94a9c8e1a732f3264ff0557f
Yoosuf <mayoosuf@gmail.com>
2018-01-22 09:58:49 +0530

    bootstrap4 update

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -10,6 +10,8 @@ window._ = require('lodash');
 try {
     window.$ = window.jQuery = require('jquery');
 
+    window.Popperjs = require('popper.js').default;
+
     require('bootstrap');
 } catch (e) {}
 
